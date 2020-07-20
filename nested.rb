2fe088a0d40e6a9c094c92b571e78b996d2ca563
Maysoon Tabali <mtab03@me.com>
2020-07-20 19:26:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
       }
     }
 
-programmer_hash => :yukihiro_matsumoto 
+programmer_hash => :yukihiro_matsumoto []
 programmer_hash => :known_for => "Ruby",
 programmer_hash => :languages => ["LISP", "C"]
   }
