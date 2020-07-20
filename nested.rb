fc42313a737b570ec09cf4b08bb7e003d08c0dda
Maysoon Tabali <mtab03@me.com>
2020-07-20 19:30:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,13 +81,16 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash => :yukihiro_matsumoto = {}
-    programmer_hash => :known_for => "Ruby",
-    programmer_hash => :languages => ["LISP", "C"]
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby",
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     programmer_hash
 end
 
-
+programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
